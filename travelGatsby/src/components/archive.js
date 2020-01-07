5e07d93c35ae3e2c61452fc82b4a2e4d2f9a704a
Stefanos <stefanosu422@gmail.com>
2020-01-06 20:43:49 -0500

    added pages programmatically using gatsby node

--- travelGatsby/src/components/archive.js
+++ travelGatsby/src/components/archive.js
@@ -1,34 +1,44 @@
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-import { render } from "react-dom"
+import { useStaticQuery, graphql, Link } from "gatsby"
 
-const Archive = () => {
-  const data = useStaticQuery(graphql`
-    query BlogPostArchive {  
-      allMarkdownRemark {
-          edges {
-            node {
-              frontmatter{
-                title
-                slug 
-              }
-            }
+const POST_ARCHIVE_QUERY = graphql`
+query BlogPostArchive {  
+  allMarkdownRemark(limit:5, sort: {
+    order:DESC
+    fields: [frontmatter___date]
+}) {
+      edges {
+        node {
+          frontmatter{
+            title
+            slug 
           }
         }
       }
-  `)
-}
-  
-  render={({allMarkdownRemark}) => (
+    }
+  }
+`
+
+const Archive = () => {
+  const data = useStaticQuery(POST_ARCHIVE_QUERY)
+  return  (
     <>
       <div>
         <aside>
           <h3>Archive</h3>
-          {allMarkdownRemark}
+          <ul> 
+            {data.allMarkdownRemark.edges.map(edge => (
+              <li key={edge.node.frontmatter.slug}>
+                <Link to={`/posts${edge.node.frontmatter.title}`}>
+                {edge.node.frontmatter.title}
+                </Link>
+              </li>
+            ))}
+          </ul>
         </aside>
       </div>
     </>
-  )}
-
+  )
+}
 
 export default Archive
